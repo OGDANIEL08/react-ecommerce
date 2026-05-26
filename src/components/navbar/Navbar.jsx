c545a49107716b34e6b31a5ac946e8f8f78f4c96
OGDANIEL08 <ogundairodaniel06@gmail.com>
2026-05-26 15:38:19 +0100

    fix  mobile navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,46 +5,32 @@ import { HiOutlineShoppingBag } from "react-icons/hi2";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState('shop')
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <div className="navbar">
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-        {!menuOpen ? '☰' : '✖' } 
+        {!menuOpen ? '☰' : '✖'}
       </div>
       <div className="logo">
         <h1>Aduke</h1>
-        <HiOutlineShoppingBag className="logo-icon"/>
+        <HiOutlineShoppingBag className="logo-icon" />
       </div>
-      
 
-    
-
-   <ul className={`nav-menu ${menuOpen ? "active" : ""}`}>
-  <li onClick={() => setMenu('shop')} className={menu === 'shop' ? 'list-s' : ''}>
-    <Link to="/" className="nav-link">Shop</Link>
-  </li>
-
-  <li onClick={() => setMenu('men')} className={menu === 'men' ? 'list-s' : ''}>
-    <Link to="/men" className="nav-link">Men</Link>
-  </li>
-
-  <li onClick={() => setMenu('women')} className={menu === 'women' ? 'list-s' : ''}>
-    <Link to="/women" className="nav-link">Women</Link>
-  </li>
-
-  <li onClick={() => setMenu('kids')} className={menu === 'kids' ? 'list-s' : ''}>
-    <Link to="/kids" className="nav-link">Kids</Link>
-  </li>
-  </ul>
+      <ul className={`nav-menu ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" className="nav-link" onClick={closeMenu}>Shop</Link></li>
+        <li><Link to="/men" className="nav-link" onClick={closeMenu}>Men</Link></li>
+        <li><Link to="/women" className="nav-link" onClick={closeMenu}>Women</Link></li>
+        <li><Link to="/kids" className="nav-link" onClick={closeMenu}>Kids</Link></li>
+      </ul>
 
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="" />
         <div className="nav-cart-count">0</div>
       </div>
-
     </div>
   );
 };
